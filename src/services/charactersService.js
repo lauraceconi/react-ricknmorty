71ec5e0b132da1584a698d74ed37a402e8a5b811
Laura Ceconi <laura.cec@hotmail.com>
2021-03-08 22:11:17 -0300

    Fix episode list call

--- src/services/charactersService.js
+++ src/services/charactersService.js
@@ -5,7 +5,7 @@ const GET_CHARACTERS = API.get(`${CHARACTERS_SLUG}`);
 
 const GET_ONE_CHARACTER = id => API.get(`${CHARACTERS_SLUG}/${id}`);
 
-const GET_EPISODES = ids => API.get(`${EPISODES_SLUG}/${ids}`);
+const GET_EPISODES = ids => API.get(`${EPISODES_SLUG}/[${ids}]`);
 
 export {
   GET_CHARACTERS,
